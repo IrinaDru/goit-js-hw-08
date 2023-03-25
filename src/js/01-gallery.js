b87ec18892e83d455f4bc5fb38698434a165cfc4
Iryna Druzheliubova <i.druzheliubova@gmail.com>
2023-03-25 17:52:50 +0200

    part-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,54 @@
+
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
 
-var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+// var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+const imgContainer = document.querySelector('.gallery');
+
+const cardsMarkup = createGalleryCardsMarkup(galleryItems); 
+
+imgContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+// console.log(creatGalleryCardsMarkup(galleryItems));
+
+
+imgContainer.addEventListener('click', onImgContainerClick);
+
+
+function createGalleryCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, description, original }) => {
+return `
+<a class="gallery__item" 
+href="${original}">
+<img class="gallery__image" 
+src="${preview}" 
+alt="${description}" />
+</a>
+    `;
+}).join('');
+
+};
+
+var lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250,});
+
+
+function onImgContainerClick (evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains('.gallery__item')) {
+        return;
+    }
+
+
+const imageUrl = evt.target.getAttribute('src');
+const instance = basicLightbox.create(`
+<img src="${imageUrl}" width="800" height="600">
+`);
+instance.show();
+}
